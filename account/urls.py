fe68c68ce7e80c6cd2a06ac08f0cfa0a6e30fe3f
U0_a124 <achouraithamiche40@gmail.com>
2017-04-14 14:32:24 +0100

    Initialize commit: 1. Create a django social network site; 2. Create an account app; 3. Styling with bootstrap; 4.Finich.

---
+++ account/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import url
+from django.contrib.auth.views import login, logout, logout_then_login
+from . import views
+
+urlpatterns = [
+    # previous login view
+    # url(r'^login/$', views.user_login, name='login'), 
+
+    # login / logout urls
+    url(r'^login/$',
+        login,
+        name='login'),
+    url(r'^logout/$',
+        logout,
+        name='logout'),
+    url(r'^logout-then-login/$', 
+        logout_then_login, 
+        name='logout_then_login'),
+    # dashboard
+    url(r'^$', views.dashboard, name='dashboard')
+]
+
