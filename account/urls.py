0a5cc465bf19ce8aa2af45cfce6344946732c392
U0_a124 <achouraithamiche40@gmail.com>
2017-04-15 17:35:33 +0100

    Add change password

--- account/urls.py
+++ account/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from django.contrib.auth.views import login, logout, logout_then_login
+from django.contrib.auth.views import login, logout, logout_then_login, password_change, password_change_done
 from . import views
 
 urlpatterns = [
@@ -7,15 +7,11 @@ urlpatterns = [
     # url(r'^login/$', views.user_login, name='login'), 
 
     # login / logout urls
-    url(r'^login/$',
-        login,
-        name='login'),
-    url(r'^logout/$',
-        logout,
-        name='logout'),
-    url(r'^logout-then-login/$', 
-        logout_then_login, 
-        name='logout_then_login'),
+    url(r'^login/$',login,name='login'),
+    url(r'^logout/$', logout,name='logout'),
+    url(r'^logout-then-login/$', logout_then_login, name='logout_then_login'),
+    url(r'^password_change/$', password_change, name='password_change'),
+    url(r'^password_change/done/$', password_change_done, name='password_change_done'),
     # dashboard
     url(r'^$', views.dashboard, name='dashboard')
 ]
